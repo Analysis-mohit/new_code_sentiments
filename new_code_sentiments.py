f108f52f54ffb6750e166357309558358bf35963
Analysis-mohit <mohit.kumar@wheelseye.com>
2025-08-25 16:22:06 +0530

    Update new_code_sentiments.py

--- new_code_sentiments.py
+++ new_code_sentiments.py
@@ -283,7 +283,7 @@ def load_and_process_data(file_path):
     """
     Loads a CSV file from the given path and preprocesses the data.
     """
-    df = pd.read_csv("sentiment_data_25aug2 - Sheet1 (1).csv")
+    df = pd.read_csv("sentiment_25aug_.csv")
     
     
     df.columns = df.columns.str.strip()
